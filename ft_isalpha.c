8d6f62e67c402869b9feecd084dbcc94f16cb822
yutabe <abe21453@cs.saisoncard.co.jp>
2023-05-17 17:02:55 +0900

    a

---
+++ ft_isalpha.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abe21453@cs.saisoncard.co.jp <abe21453@    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 16:06:04 by abe21453@cs       #+#    #+#             */
+/*   Updated: 2023/05/17 16:56:30 by abe21453@cs      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+#include <stdio.h>
+
+int	ft_isalpha(char c)
+{
+	if ((97 <= c && c <= 122) || (65 <= c && c <= 90))
+		return (1);
+	else
+		return (0);
+}
+
+int	main(void)
+{
+	char c = 'a';
+	printf("%d\n", ft_isalpha(c));
+	printf("%d\n", isalpha(c));
+
+    printf("%d\n", ft_isalpha('A'));
+    printf("%d\n", isalpha('A'));
+    
+    printf("%d\n", ft_isalpha('Z'));
+    printf("%d\n", isalpha('Z'));
+
+    printf("%d\n", ft_isalpha('z'));
+    printf("%d\n", isalpha('z'));
+
+    printf("%d\n", ft_isalpha('0'));
+    printf("%d\n", isalpha('0'));
+
+    printf("%d\n", ft_isalpha('9'));
+    printf("%d\n", isalpha('9'));
+
+    printf("%d\n", ft_isalpha(' '));
+    printf("%d\n", isalpha(' '));
+
+}
