f7070afbddeaa3f679568a4a655c7e42094e7d9c
yutabe <abe21453@cs.saisoncard.co.jp>
2023-05-17 20:11:07 +0900

    a

---
+++ Makefile
@@ -0,0 +1,24 @@
+NAME = libft.a
+SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum
+OBJS = $(SRCS:%.c=%.o)
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	ar rcs $(NAME) $(OBJS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME) $(TARGET)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,14 +6,15 @@
 /*   By: abe21453@cs.saisoncard.co.jp <abe21453@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 16:06:04 by abe21453@cs       #+#    #+#             */
-/*   Updated: 2023/05/17 16:56:30 by abe21453@cs      ###   ########.fr       */
+/*   Updated: 2023/05/17 19:17:39 by abe21453@cs      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctype.h>
-#include <stdio.h>
 
-int	ft_isalpha(char c)
+// #include <stdio.h>
+
+int	ft_isalpha(int c)
 {
 	if ((97 <= c && c <= 122) || (65 <= c && c <= 90))
 		return (1);
@@ -21,28 +22,28 @@ int	ft_isalpha(char c)
 		return (0);
 }
 
-int	main(void)
-{
-	char c = 'a';
-	printf("%d\n", ft_isalpha(c));
-	printf("%d\n", isalpha(c));
+// int	main(void)
+// {
+// 	char c = 'a';
+// 	printf("%d\n", ft_isalpha(c));
+// 	printf("%d\n", isalpha(c));
 
-    printf("%d\n", ft_isalpha('A'));
-    printf("%d\n", isalpha('A'));
-    
-    printf("%d\n", ft_isalpha('Z'));
-    printf("%d\n", isalpha('Z'));
+//     printf("%d\n", ft_isalpha('A'));
+//     printf("%d\n", isalpha('A'));
 
-    printf("%d\n", ft_isalpha('z'));
-    printf("%d\n", isalpha('z'));
+//     printf("%d\n", ft_isalpha('Z'));
+//     printf("%d\n", isalpha('Z'));
 
-    printf("%d\n", ft_isalpha('0'));
-    printf("%d\n", isalpha('0'));
+//     printf("%d\n", ft_isalpha('z'));
+//     printf("%d\n", isalpha('z'));
 
-    printf("%d\n", ft_isalpha('9'));
-    printf("%d\n", isalpha('9'));
+//     printf("%d\n", ft_isalpha('0'));
+//     printf("%d\n", isalpha('0'));
 
-    printf("%d\n", ft_isalpha(' '));
-    printf("%d\n", isalpha(' '));
+//     printf("%d\n", ft_isalpha('9'));
+//     printf("%d\n", isalpha('9'));
 
-}
+//     printf("%d\n", ft_isalpha(' '));
+//     printf("%d\n", isalpha(' '));
+
+// }
