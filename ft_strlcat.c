d4660fb535a371bc1804032b5cf78eaeea6438f6
Yuta Abe <yutabe@c1r21s5.42tokyo.jp>
2023-05-20 20:18:34 +0900

    a

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: yutabe <yutabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 17:12:41 by yutabe            #+#    #+#             */
-/*   Updated: 2023/05/20 20:07:29 by yutabe           ###   ########.fr       */
+/*   Updated: 2023/05/20 20:16:28 by yutabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 int	main(void)
 {
-	char str[20] = "123456789";
-	char str2[10] = "abcde";
-	char str3[20] = "123456789";
-	char str4[10] = "abcde";
+	char str[10] = "123456789";
+	char str2[6] = "abcde";
+	char str3[10] = "123456789";
+	char str4[6] = "abcde";
 
-	printf("ft_strlcat:%lu\n", ft_strlcat(str, str2, 8));
+	printf("ft_strlcat:%lu\n", ft_strlcat(str, str2, 10));
 	printf("ft_strlcat:%s\n", str);
-	printf("strlcat:%lu\n", strlcat(str3, str4, 8));
+	printf("strlcat:%lu\n", strlcat(str3, str4, 10));
 	printf("strlcat:%s\n", str3);
 }
