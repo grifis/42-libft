990fdef282e9fe741dbc6335caf6e732a02ae4a6
Yuta Abe <yutabe@c1r21s5.42tokyo.jp>
2023-05-20 19:00:39 +0900

    a

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: yutabe <yutabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 17:12:41 by yutabe            #+#    #+#             */
-/*   Updated: 2023/05/20 18:31:04 by yutabe           ###   ########.fr       */
+/*   Updated: 2023/05/20 19:00:12 by yutabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	const char	*s;
 	int			limit;
-	const int dst_val = strlen(dst);
+	int			dst_val;
 
 	s = src;
+	dst_val = ft_strlen(dst);
 	limit = dstsize - strlen(dst) - 1;
-	if (dstsize < 0)
-		return (ft_strlen(src) + dst_val);
+	if (dstsize < ft_strlen(src))
+		return (ft_strlen(src) + dstsize);
 	while (limit-- && *s)
 	{
 		*dst = *s;
@@ -41,8 +42,8 @@ int	main(void)
 	char str3[20] = "123456789";
 	char str4[10] = "abcde";
 
-	printf("ft_strlcat:%lu\n", ft_strlcat(str, str2, 15));
+	printf("ft_strlcat:%lu\n", ft_strlcat(str, str2, 20));
 	printf("ft_strlcat:%s\n", str);
-	printf("strlcat:%lu\n", strlcat(str3, str4, 15));
+	printf("strlcat:%lu\n", strlcat(str3, str4, 20));
 	printf("strlcat:%s\n", str2);
 }
