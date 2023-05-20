a2ea7e8d94177a429a7327c15851bb065fe27eeb
Yuta Abe <yutabe@c1r21s5.42tokyo.jp>
2023-05-20 20:43:40 +0900

    a

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: yutabe <yutabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 17:12:41 by yutabe            #+#    #+#             */
-/*   Updated: 2023/05/20 20:40:01 by yutabe           ###   ########.fr       */
+/*   Updated: 2023/05/20 20:43:35 by yutabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	char		*d;
 	const char	*s;
 	int			writable;
-	int			dst_n;
+	int			d_n;
 
-	writable = dstsize - ft_strlen(dst) - 1;
-	if (dstsize <= ft_strlen(dst))
+	if (dst)
+		d_n = ft_strlen(dst);
+	writable = dstsize - d_n - 1;
+	if (dstsize <= d_n)
 		return (ft_strlen(src) + dstsize);
 	d = dst;
 	s = src;
-	dst_n = ft_strlen(dst);
 	while (*d != '\0')
 		d++;
 	while (writable-- && *s)
@@ -36,18 +37,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		s++;
 	}
 	*d = '\0';
-	return (ft_strlen(src) + dst_n);
+	return (ft_strlen(src) + d_n);
 }
 
 int	main(void)
 {
-	// char str[10] = "123456789";
-	// char str2[6] = "abcde";
+	char str[10] = "123456789";
+	char str2[6] = "abcde";
 	char str3[10] = "123456789";
 	char str4[6] = "abcde";
 
-	// printf("ft_strlcat:%lu\n", ft_strlcat(NULL, str2, 0));
-	// printf("ft_strlcat:%s\n", str);
+	printf("ft_strlcat:%lu\n", ft_strlcat(NULL, str2, 0));
+	printf("ft_strlcat:%s\n", str);
 	printf("strlcat:%lu\n", strlcat(NULL, str4, 0));
 	printf("strlcat:%s\n", str3);
 }
