8cad3db42d016c0ba9805680798ec21f3b5a57e3
grifis <nekogrifis@gmail.com>
2023-05-21 00:02:34 +0900

    a

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,53 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yutabe <yutabe@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abeyuuta <abeyuuta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 17:12:41 by yutabe            #+#    #+#             */
-/*   Updated: 2023/05/20 21:21:40 by yutabe           ###   ########.fr       */
+/*   Updated: 2023/05/20 23:59:03 by abeyuuta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	char		*d;
-	const char	*s;
-	int			writable;
-	int			d_n;
+	size_t	d_len;
+	size_t	s_len;
+	size_t	i;
 
 	if (!dst && dstsize == 0)
 		return (ft_strlen(src));
-	d_n = ft_strlen(dst);
-	if ((int)dstsize <= d_n)
-		return (ft_strlen(src) + dstsize);
-	d = dst;
-	s = src;
-	while (*d != '\0')
-		d++;
-	writable = dstsize - d_n - 1;
-	while (writable-- && *s)
+	d_len = ft_strlen(dst);
+	s_len = ft_strlen(src);
+	if (dstsize <= d_len)
+		return (s_len + dstsize);
+	i = 0;
+	while (src[i] && (d_len + i + 1) < dstsize)
 	{
-		*d = *s;
-		d++;
-		s++;
+		dst[d_len + i] = src[i];
+		i++;
 	}
-	*d = '\0';
-	return (ft_strlen(src) + d_n);
+	dst[d_len + i] = '\0';
+	return (d_len + s_len);
 }
 
-int	main(void)
-{
-	char str[10] = "123456789";
-	char str2[6] = "abcde";
-	char str3[10] = "123456789";
-	char str4[6] = "abcde";
+// int	main(void)
+// {
+// 	char str[10] = "123456789";
+// 	char str2[6] = "abcde";
+// 	char str3[10] = "123456789";
+// 	char str4[6] = "abcde";
 
-	printf("ft_strlcat:%lu\n", ft_strlcat(NULL, str2, 0));
-	printf("ft_strlcat:%s\n", str);
-	printf("strlcat:%lu\n", strlcat(NULL, str4, 0));
-	printf("strlcat:%s\n", str3);
-}
+// 	printf("ft_strlcat:%lu\n", ft_strlcat(NULL, str2, 0));
+// 	printf("ft_strlcat:%s\n", str);
+// 	printf("strlcat:%lu\n", strlcat(NULL, str4, 0));
+// 	printf("strlcat:%s\n", str3);
+// }
